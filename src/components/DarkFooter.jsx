72780f24c402c662a79a30a660d5d60a1f83fdde
Khadeem Bernard <89490904+AI-Known-As-Bernard@users.noreply.github.com>
2024-11-20 18:27:35 -0700

    Now the header
    Finished the footer and now working on the header

--- src/components/DarkFooter.jsx
+++ src/components/DarkFooter.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaInstagram, FaTwitter, FaGithub } from "react-icons/fa";
 const DarkFooter = () => {
     let navigation = {
         logo: "https://res.cloudinary.com/west-mec-north-east-campus-coding/image/upload/v1652816912/NewsLetter/WM-Coding_b2q0dl.svg",
-        message: "The Motto is to motto for life just not a day",
+        message: "Code, Create, Conquer – Fun Every Step of the Way!",
         socialMedia: [
             {
                 name: "Facebook",
@@ -31,40 +31,40 @@ const DarkFooter = () => {
             { name: 'About', url: '/about' },
             { name: 'Meet the Team', url: '/meet-the-team' },
             { name: 'History/Testimonials', url: '/history-testimonials' },
-            { name: 'Careers', url: '/careers' }
+            { name: 'Careers Options', url: '/careers' }
           ],
         helpfulLinks:[
             { name: 'Contact', url: '/contact' },
             { name: 'FAQ', url: '/faq' },
-            { name: 'Live Caht', url: '/live-caht' }
           ],
         legal:[
             { name: 'Privacy Policy', url: '/privacy-policy' },
             { name: 'Terms & Conditions', url: '/terms---conditions' },
-            { name: 'Return Policy', url: '/return-policy' },
-            { name: 'Accessibility', url: '/accessibility' }
           ],
         services:[
-            { name: 'Service1', url: '/service1' },
-            { name: 'Service2', url: '/service2' },
-            { name: 'Service3', url: '/service3' },
-            { name: 'Service4', url: '/service4' }
+            { name: 'Current Students', url: '/students' },
+            { name: 'Alumni', url: '/alumni' },
+            { name: 'Blog', url: '/blog' },
+            { name: 'FBLA', url: '/fbla' }
           ],
-        companyName:"Umbrella Company",
+        companyName:"West-MEC North-East Coding -",
         year:2024
     }
   return (
     <footer className="bg-gray-900">
-      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-12 sm:pt-24 lg:px-8 lg:pt-32">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8">
-            <img
-              alt="Company name"
-              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-              className="h-9"
-            />
-            <p className="text-balance text-sm/6 text-gray-300">
-              Making the world a better place through constructing elegant hierarchies.
+            <span className="flex items-start">
+                <img
+                alt={navigation.companyName}
+                src={navigation.logo}
+                className="h-16"
+                />
+                <p className="text-white text-xl font-bold">North-East Coding</p>
+            </span>
+            <p className="text-balance text-sm font-bold text-gray-300">
+              {navigation.message}
             </p>
             <div className="flex gap-x-6">
               {navigation.socialMedia.map((item) => (
@@ -78,7 +78,7 @@ const DarkFooter = () => {
           <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
             <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
-                <h3 className="text-sm/6 font-semibold text-white">Services</h3>
+                <h3 className="text-sm/6 font-semibold text-white">Site Map</h3>
                 <ul className="mt-6 space-y-4">
                   {navigation.services.map((item) => (
                     <li key={item.name}>
@@ -131,7 +131,7 @@ const DarkFooter = () => {
           </div>
         </div>
         <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
-          <p className="text-sm/6 text-gray-400">&copy; 2024 Your Company, Inc. All rights reserved.</p>
+          <p className="text-sm/6 text-gray-400">&copy; {navigation.year} {navigation.companyName} All rights reserved.</p>
         </div>
       </div>
     </footer>
--- src/pages/layouts/MainLayout.jsx
+++ src/pages/layouts/MainLayout.jsx
@@ -1,13 +1,26 @@
 import DarkFooter from "../../components/DarkFooter"
+import SimpleNavbar from "../../components/SimpleNavbar"
 
 const MainLayout = ({ children }) =>{
     return(
+        <>
         <div>
+            <SimpleNavbar/> 
            <header>Header Content</header> 
            <main>{children}</main>
            <DarkFooter/>
         </div>
-    )
+        <script>
+  const menuToggle = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobile-menu')
+
+  menuToggle.addEventListener('click', function () {
+    mobileMenu.classList.toggle('hidden')
+  })
+</script>
+        </>
+
+)
 }
 
 export default MainLayout
